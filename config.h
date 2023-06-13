99890f42b02ffe47e905c45eeaef1e5ff06b623e
jotheesh1729 <66893484+jotheesh1729@users.noreply.github.com>
2023-06-13 05:05:37 +0200

    config.h

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
-#define IO_USERNAME  "Jotheesh"  // Adafruit IO username
-#define IO_KEY       "aio_udDx2601KHLxbsFhqMuXhos2dfvn"  // Adafruit IO key
+#define IO_USERNAME  " "  // Adafruit IO username
+#define IO_KEY       " "  // Adafruit IO key
 
-char ssid1[] = "TP-Link_90E87";  // SSID of WiFi network 1
-char pass1[] = "73255406";  // Password of WiFi network 1
-char ssid3[] = "admins";  // SSID of WiFi network 3
-char pass3[] = "19271477";  // Password of WiFi network 3
-char ssid2[] = "Qwe";  // SSID of WiFi network 2
-char pass2[] = "123456789";  // Password of WiFi network 2
+char ssid1[] = "ssid1";  // SSID of WiFi network 1
+char pass1[] = "pass1";  // Password of WiFi network 1
+char ssid3[] = "ssid3";  // SSID of WiFi network 3
+char pass3[] = "pass3";  // Password of WiFi network 3
+char ssid2[] = "ssid2";  // SSID of WiFi network 2
+char pass2[] = "pass2";  // Password of WiFi network 2
 
 const char* ssids[] = {ssid1, ssid2, ssid3};  // Array of SSIDs
 const char* passwords[] = {pass1, pass2, pass3};  // Array of corresponding passwords
